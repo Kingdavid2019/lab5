e5768ea412a7e13b3bb85081ca101f6d170ca820
Kingdavid2019 <david.b.aycock-1@ou.edu>
2019-02-19 23:27:42 -0600

    finished

---
+++ src/Todo.java
@@ -0,0 +1,60 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2018-02-19
+OU ID_______________: 113465086
+OU EMAIL____________: david.b.aycock-1@ou.edu
+
+Tasks:
+1. calculateTwoTokens 
+2. calculateThreeTokens 
+3. execute method
+4. parseAndExecute method 
+5. calculateTwoTokensTestValidHalve 
+6. calculateTwoTokensTestInvalidCommand
+7. calculateThreeTokensTestValidAdd
+8. calculateThreeTokensTestValidSubtract
+9. calculateThreeTokensTestValidDivide 
+10. calculateThreeTokensTestInvalidCommand
+11. executeTestValidQuit
+12. executeTestValidThreeTokens
+13. executeTestInvalidTokenLength
+14. parseAndExecuteTestDivideByZero
+15. parseAndExecuteTestInvalidNumber
+16. parseAndExecuteTestInvalidTokenLength
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 5
+6. 5 
+7. 5
+8. 5 
+9. 5
+10. 5
+11. 5
+12. 5
+13. 5 
+14. 5
+15. 5
+16. 5
+
+Actual Time (minutes):
+1. 10
+2. 10
+3. 15
+4. 10
+5. 10
+6. 7
+7. 7
+8. 7
+9. 7
+10. 7
+11. 7
+12. 3
+13. 3
+14. 3 
+15. 3
+16. 3
+*/}
